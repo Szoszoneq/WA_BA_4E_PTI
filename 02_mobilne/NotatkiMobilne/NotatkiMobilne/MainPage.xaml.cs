5deea0178125dfa487c190bd6d49e45bf2c8730c
Szoszoneq <bartosztomaszw@gmail.com>
2026-09-25 11:06:47 +0200

    02_mobilne – projekt C# .NET MAUI NotatkiMobilne

---
+++ 02_mobilne/NotatkiMobilne/NotatkiMobilne/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NotatkiMobilne
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Notatki { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Notatki = new ObservableCollection<string>
+            {
+                "Kupić mleko i chleb",
+                "Wizyta u weterynarza o 16:00",
+                "Przegląd samochodu"
+            };
+
+            NotatkiListView.ItemsSource = Notatki;
+        }
+
+        private void OnDodajClicked(object sender, EventArgs e)
+        {
+            string tekstNotatki = NotatkaEntry.Text;
+            if (!string.IsNullOrWhiteSpace(tekstNotatki))
+            {
+                Notatki.Add(tekstNotatki.Trim());
+
+                NotatkaEntry.Text = string.Empty;
+            }
+        }
+    }
+}
