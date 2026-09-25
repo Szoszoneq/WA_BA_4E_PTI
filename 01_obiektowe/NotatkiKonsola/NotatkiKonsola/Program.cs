949e0f3fcf7dc9e227e728769d7fa9be9e0362c7
Szoszoneq <bartosztomaszw@gmail.com>
2026-09-25 11:04:52 +0200

    01_obiektowe – projekt C# NotatkiKonsola

---
+++ 01_obiektowe/NotatkiKonsola/NotatkiKonsola/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/*
+Nazwa: Notatka
+Opis: Klasa reprezentuje notatke
+Pola: licznik,id,tresc i tytul 
+Metody: Notatka, wyswietl, diagnotsyka, 
+Autor: Waleczek Bartosz
+*/
+public class Notatka
+{
+    private static int licznik;
+    private int id;
+    protected string tresc;
+    protected string tytul;
+
+    public Notatka(string tytulP, string trescP){
+        licznik++;
+        id = licznik;
+        tytul = tytulP;
+        tresc = trescP;
+    }
+    public void wyswietl()
+    {
+        Console.WriteLine("------Notatka"+id+"------");
+        Console.WriteLine(tytul);
+        Console.WriteLine(tresc);
+        Console.WriteLine("-------------------");
+    }
+    public void diagnostyka()
+    {
+        Console.WriteLine("---------------------------Diagnostyka"+id+"-----------------------------");
+        Console.WriteLine("Licznik:"+licznik+";"+"Id:"+id+";"+"Tytul:"+tytul+";"+"Tresc:"+tresc);
+        Console.WriteLine("-------------------------------------------------------------------------");
+    }
+    public static void Main()
+    {
+        Notatka n1 = new Notatka("Plan lekcji", "Matematyka, Polski, Informatyka, Pracownia projektowania obiektowego");
+        Notatka n2 = new Notatka("Lista zakupów", "Chleb, Masło, Szynka, Ser, Mleko");
+        n1.wyswietl();
+        n1.diagnostyka();
+        n2.wyswietl();
+        n2.diagnostyka();
+
+    }
+}
+
